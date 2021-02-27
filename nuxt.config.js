c1f9eb687de38858370dbcea09250f8b4ef6d485
44 <a44@44noMacBook-Pro.local>
2021-02-28 03:58:57 +0900

    update title

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,9 @@ export default {
    ** See https://nuxtjs.org/api/configuration-head
    */
   head: {
-    title: process.env.npm_package_name || "",
+    title:
+      process.env.npm_package_name ||
+      "ウマ娘プリティーダービー 編成サポートツール",
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
